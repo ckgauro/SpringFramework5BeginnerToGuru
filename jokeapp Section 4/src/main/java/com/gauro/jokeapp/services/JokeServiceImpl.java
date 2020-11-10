e45ae55b8bd2858ac143dbca7a3dacbb290f370f
Chandra kishor Gauro <ckgauro@gmail.com>
2020-11-09 21:11:31 -0500

    joke Section 4

---
+++ jokeapp Section 4/src/main/java/com/gauro/jokeapp/services/JokeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.gauro.jokeapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Chandra
+ */
+@Service
+public class JokeServiceImpl  implements JokeService{
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
